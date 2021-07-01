c2b0af49078c5deceb268c848437274e2a26572c
mrbusysky <58412572+mrbusysky@users.noreply.github.com>
2021-07-01 15:02:16 -0700

    Update popup.js

--- js/wallet/popup/popup.js
+++ js/wallet/popup/popup.js
@@ -4,7 +4,7 @@ let custom_created = false;
 let manageKey,
 	getPref = false;
 let walletsList = new WalletsList();
-let defaultBank = 'http://54.177.121.3';
+let defaultBank = 'http://13.233.77.254';
 
 /**
  * Temporary workaround for secondary monitors on MacOS where redraws don't happen
